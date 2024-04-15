4b429724fc770ae266e9489aa05c81ceb93de4dd
bigprice <bigangelito@gmail.com>
2024-04-15 11:35:45 -0600

    implement mediator in catalogapi

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreatePoductHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+
+        
+
+        throw new NotImplementedException();
+    }
+}
